0e83f9f4be3cb17d74687978249cc15c24b4a0c0
ldccccc <glzx1@student.london.ac.uk>
2024-07-16 15:37:28 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -56,8 +56,8 @@ import { useRouter } from 'vue-router'
 import { ElScrollbar, ElConfigProvider } from 'element-plus'
 import zhCn from "element-plus/es/locale/lang/zh-cn"
 
-const repoLink = 'https://blog.lianglianglee.com'
-const repoName = '墓灵守护'
+const repoLink = 'https://cloudflare.com'
+const repoName = 'cloudflare支持'
 const appName = '图床管理工具'
 
 document.title = appName
